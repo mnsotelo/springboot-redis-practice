5cc3c0be0cc2bdd6841927bc9a3bfc279a30f886
Martin Sotelo <martinsoteloferry.inf3@gmail.com>
2024-03-04 12:19:05 +0100

    first commit

---
+++ src/test/java/com/example/redispractice/Redis/practice/RedisPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redispractice.Redis.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
